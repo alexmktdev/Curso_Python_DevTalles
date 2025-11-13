005dc7cb7ff947d3678a31df6b6dcfe7b81799c1
Alexander Ruiz Poblete <alexanderzananiri26@gmail.com>
2025-11-12 22:49:28 -0300

    Se agrega ejemplo02_comentarios

---
+++ 01-primeros-pasos/02_comentarios.py
@@ -0,0 +1,16 @@
+# comentarios en python 
+
+# comentarios de una linea en python (no influyen en el codigo)
+
+"""
+ este es un comentario 
+ multilinea
+ usando comillas dobles 
+"""
+
+'''
+este es un comentario
+multilinea pero
+esta vez usando comillas simples
+ '''
+
