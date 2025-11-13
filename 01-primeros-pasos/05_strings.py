e378a3c201b01bbe71ca41c8b30c9f9397c2b550
Alexander Ruiz Poblete <alexanderzananiri26@gmail.com>
2025-11-13 00:11:52 -0300

    se agregan los ultimos dos ejemplos el 05 y 06

---
+++ 01-primeros-pasos/05_strings.py
@@ -0,0 +1,22 @@
+# Strings en Python 
+
+texto1 = "hola"
+texto2 = 'holaaa'
+
+# otro tipo de strings - multilinea
+texto3 = '''
+         hola , soy alexander y
+         bla bla bla
+         adios !
+        '''
+
+print(texto3)
+
+
+# concatenar strings 
+nombre = "Alexander"
+apellido = "Ruiz"
+espacio = " "
+
+nombre_completo = nombre + espacio + apellido
+print(nombre_completo)
---
+++ 01-primeros-pasos/06_f_strings.py
@@ -0,0 +1,13 @@
+# f -strings en python 
+
+nombre = "Alexander"
+apellido = "Ruiz"
+
+nombre_completo = f"{nombre} {apellido}"
+
+print(nombre_completo)
+
+edad = 31 
+
+mensaje = f"{nombre_completo} tienes {edad} años"
+print(mensaje)
