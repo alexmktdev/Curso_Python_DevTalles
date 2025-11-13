d7aa292f0ed80e9c55c3f1f608b5f4cfac9a8eb4
Alexander Ruiz Poblete <alexanderzananiri26@gmail.com>
2025-11-13 00:49:56 -0300

    se agrega el ejemplo11

---
+++ 01-primeros-pasos/11_funciones_nativas.py
@@ -0,0 +1,8 @@
+# funciones incorporadas o nativas
+
+# print
+# type
+
+mensaje = "Es un buen programador"
+
+mensaje.replace("Es", "Soy") # metodo reemplazar 
---
+++ 01-primeros-pasos/12_inputs.py
@@ -0,0 +1,2 @@
+# inputs por teclado 
+
