d83971cc447d65459f03a1ff11561e96c6ebd5c0
Alexander Ruiz Poblete <alexanderzananiri26@gmail.com>
2025-11-12 23:01:24 -0300

    se agrega el ejemplo04  tipos_de_datos

---
+++ 01-primeros-pasos/04_tipos_de_datos.py
@@ -0,0 +1,39 @@
+# tipos de datos en python 
+
+# String - cadenas de texto 
+nombre = "nicolas"
+saludo = "hola a todos"
+numero_string = "20"
+
+# int - integer - enteros
+edad = 20
+numero_de_personas = 12
+
+# float - decimales 
+temperatura = 20.5
+dinero = 9.99
+
+# booleanos - estados (true o false)
+esta_prendido = True
+esta_apagado = False
+
+
+# Datos especiales 
+
+# listas
+lista = [1,2,3,4,5] # coleccion de numeros 
+lista_nombres = ['nicolas', 'fabian', 'fernanda'] # coleccion de strings
+
+# tuplas 
+tupla = (1,2,3) # datos en grupos, agrupados
+
+# sets
+mi_set = {1,2,3} # coleccion qe no se repiten datos
+
+# diccionarios
+mi_diccionario = {
+    'nombre': 'ricardo',
+    'apellido': 'Ruiz',
+    'rut': 18679045-6,
+}
+
