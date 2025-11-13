be9de8ebe4986f298c783fff747aa6474f0fe3af
Alexander Ruiz Poblete <alexanderzananiri26@gmail.com>
2025-11-12 22:55:29 -0300

    se agrega el ejemplo03_variables

---
+++ 01-primeros-pasos/03_variables.py
@@ -0,0 +1,24 @@
+# variables en python 
+
+saludo = "Hola Mundo" # variable saludo guarda el String "Hola Mundo"
+
+print(saludo) # imprimimos la variable saludo
+
+nombre = "Alexander" # variable nombre guarda el String "Alexander"
+
+print(nombre)
+
+edad = 25   # variable entero
+print(edad)
+
+dinero = 25.99 # variable float o decimal 
+print(dinero)
+
+# pep8 - buenas practicas de Python
+# las variables en python, se escriben en snake_case 
+
+_variable_privada = "variable" # se usa en POO, mas adelante se verá
+
+
+
+
