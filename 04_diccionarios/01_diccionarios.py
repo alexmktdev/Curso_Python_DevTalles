49922d0bde592b39490b0002c7753e590445819a
Alexander Ruiz Poblete <alexanderzananiri26@gmail.com>
2025-11-14 00:24:17 -0300

    Se agrega el ejemplo1 de 04-diccionarios

---
+++ 04_diccionarios/01_diccionarios.py
@@ -0,0 +1,14 @@
+# diccionarios en python 
+
+# no estan ordenados 
+
+diccionario = {
+    'key': 0,
+    'key2': [1,2,3],
+    'key3': (2.4, 3.6),
+    'key4': "Diccionario"
+}
+
+# accedamos a un elemento del diccionario 
+print(diccionario['key']) # se llama al nombre['clave que quieres']
+print(diccionario['key4'])
